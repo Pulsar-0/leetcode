fc3398bedae81400a99a9673a8671c03ae4fb6dc
pulsar <nilumilakplay@gmail.com>
2025-02-17 15:55:32 +0300

    added solution for problem 13

---
+++ 13_romanToInteger.py
@@ -0,0 +1,23 @@
+def romanToInt(self, s):
+	romans = {
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000
+	}
+	sum = 0
+	last = 0
+
+	for symbol in s:
+		num = romans.get(symbol)
+
+		if last < num:
+			num -= last * 2
+		
+		sum += num
+		last = num
+
+	return sum
