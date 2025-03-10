a423d1e2565e843f91205611f47eaf79da844fcb
pulsar <nilumilakplay@gmail.com>
2025-03-10 11:37:14 +0300

    now you can run the script to test the function

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -1,9 +1,39 @@
-def twoSum(self, nums, target):
-	n = len(nums)
+class Solution(object):
+	def twoSum(self, nums, target):
+		n = len(nums)
 
-	for i in range(n - 1):
-		for j in range(i + 1, n):
-			if nums[i] + nums[j] == target:
-				return [i, j]
+		for i in range(n - 1):
+			for j in range(i + 1, n):
+				if nums[i] + nums[j] == target:
+					return [i, j]
 
-	return []
+		return []
+
+'''
+basic test cases
+you can add yours here
+indexes must match in each list
+'''
+testCaseNums = [
+	[2, 7, 11, 15],
+	[3, 2, 4],
+	[3, 3],
+]
+testCaseTargets = [
+	9, 6, 6,
+]
+testCaseExpected = [
+	[0, 1],
+	[1, 2],
+	[0, 1],
+]
+
+if __name__ == '__main__':
+	solution = Solution()
+	for i in range(0, len(testCaseNums)):
+		case = i + 1
+
+		if solution.twoSum(testCaseNums[i], testCaseTargets[i]) == testCaseExpected[i]:
+			print(f"case {case}: success")
+		else:
+			print(f"case {case}: failure")
